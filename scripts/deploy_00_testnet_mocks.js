a071ee4a60db207a0d8f54e8e5d212fb5adc4009
msingi <gobaseyourself@gmail.com>
2020-11-27 16:37:03 -0600

    Update deployment scripts

--- scripts/deploy_00_testnet_mocks.js
+++ scripts/deploy_00_testnet_mocks.js
@@ -1,5 +1,6 @@
 const bre = require('@nomiclabs/buidler')
 const { ethers, upgrades } = bre
+const { getSavedContractAddresses, saveContractAddress } = require('./utils')
 
 async function main() {
     await bre.run('compile')
@@ -8,15 +9,18 @@ async function main() {
     const lpToken = await upgrades.deployProxy(ERC20UpgradeSafe, [])
     await lpToken.deployed()
     console.log('LP token deployed to:', lpToken.address)
+    saveContractAddress(bre.network.name, 'lpToken', lpToken.address)
 
     const MockOracle = await ethers.getContractFactory('MockOracle')
     const mcapOracle = await MockOracle.deploy('Mcap oracle')
     await mcapOracle.deployed()
     console.log('Market cap oracle deployed to:', mcapOracle.address)
+    saveContractAddress(bre.network.name, 'mcapOracle', mcapOracle.address)
 
     const tokenPriceOracle = await MockOracle.deploy('Token price oracle')
     await tokenPriceOracle.deployed()
     console.log('Token price oracle deployed to:', tokenPriceOracle.address)
+    saveContractAddress(bre.network.name, 'tokenPriceOracle', tokenPriceOracle.address)
 }
 
 
--- scripts/deploy_01_deploy_contracts.js
+++ scripts/deploy_01_deploy_contracts.js
@@ -1,56 +1,60 @@
 const bre = require('@nomiclabs/buidler')
 const { ethers, upgrades } = bre
+const { getSavedContractAddresses, saveContractAddress } = require('./utils')
 
 async function main() {
-    console.log(bre)
     await bre.run('compile')
 
     const BaseToken = await ethers.getContractFactory('BaseToken')
     const baseToken = await upgrades.deployProxy(BaseToken, [])
     await baseToken.deployed()
     console.log('BaseToken deployed to:', baseToken.address)
+    saveContractAddress(bre.network.name, 'baseToken', baseToken.address)
 
     const BaseTokenMonetaryPolicy = await ethers.getContractFactory('BaseTokenMonetaryPolicy')
     const baseTokenMonetaryPolicy = await upgrades.deployProxy(BaseTokenMonetaryPolicy, [baseToken.address, '483703019126'])
     await baseTokenMonetaryPolicy.deployed()
     console.log('BaseTokenMonetaryPolicy deployed to:', baseTokenMonetaryPolicy.address)
+    saveContractAddress(bre.network.name, 'baseTokenMonetaryPolicy', baseTokenMonetaryPolicy.address)
 
     const BaseTokenOrchestrator = await ethers.getContractFactory('BaseTokenOrchestrator')
     const baseTokenOrchestrator = await upgrades.deployProxy(BaseTokenOrchestrator, [baseTokenMonetaryPolicy.address])
     await baseTokenOrchestrator.deployed()
     console.log('BaseTokenOrchestrator deployed to:', baseTokenOrchestrator.address)
+    saveContractAddress(bre.network.name, 'baseTokenOrchestrator', baseTokenOrchestrator.address)
 
     const Cascade = await ethers.getContractFactory('Cascade')
     const cascade = await upgrades.deployProxy(Cascade, [])
     await cascade.deployed()
     console.log('Cascade deployed to:', cascade.address)
+    saveContractAddress(bre.network.name, 'cascade', cascade.address)
 
     await (await baseToken.setMonetaryPolicy(baseTokenMonetaryPolicy.address)).wait()
-    console.log('BaseToken.setMonetaryPolicy() succeeded')
+    console.log('BaseToken.setMonetaryPolicy(', baseTokenMonetaryPolicy.address, ') succeeded')
     await (await baseTokenMonetaryPolicy.setOrchestrator(baseTokenOrchestrator.address)).wait()
-    console.log('BaseTokenMonetaryPolicy.setOrchestrator() succeeded')
+    console.log('BaseTokenMonetaryPolicy.setOrchestrator(', baseTokenOrchestrator.address, ') succeeded')
 
-    const externalContracts = {
-        mainnet: {
-            mcapOracle:  '0xEC8761a0A73c34329CA5B1D3Dc7eD07F30e836e2',
-            priceOracle: '',
-            lpToken:     '',
-        },
-        kovan: {
-            mcapOracle:  '0xcD1AA31a9fDD89D21c0104DB6f6A46C8FE271D3b',
-            priceOracle: '0x3C71Acc6F1ed1cF67b6a77345F8ee0467E94cD40',
-            lpToken:     '0x141819E5aB1FA056fe5da5dE735Ba1E82D1A7d53',
-        },
-    }
+    // const externalContracts = {
+    //     mainnet: {
+    //         mcapOracle:  '0xEC8761a0A73c34329CA5B1D3Dc7eD07F30e836e2',
+    //         priceOracle: '',
+    //         lpToken:     '',
+    //     },
+    //     kovan: {
+    //         mcapOracle:  '0xcD1AA31a9fDD89D21c0104DB6f6A46C8FE271D3b',
+    //         priceOracle: '0x3C71Acc6F1ed1cF67b6a77345F8ee0467E94cD40',
+    //         lpToken:     '0x141819E5aB1FA056fe5da5dE735Ba1E82D1A7d53',
+    //     },
+    // }
 
-    const contracts = externalContracts[bre.network.name]
+    const contracts = getSavedContractAddresses()[bre.network.name]
 
     await (await baseTokenMonetaryPolicy.setMcapOracle(contracts.mcapOracle)).wait()
-    console.log('BaseTokenMonetaryPolicy.setMcapOracle() succeeded')
-    await (await baseTokenMonetaryPolicy.setTokenPriceOracle(contracts.priceOracle)).wait()
-    console.log('BaseTokenMonetaryPolicy.setTokenPriceOracle() succeeded')
+    console.log('BaseTokenMonetaryPolicy.setMcapOracle(', contracts.mcapOracle, ') succeeded')
+    await (await baseTokenMonetaryPolicy.setTokenPriceOracle(contracts.tokenPriceOracle)).wait()
+    console.log('BaseTokenMonetaryPolicy.setTokenPriceOracle(', contracts.tokenPriceOracle, ') succeeded')
     await (await cascade.setLPToken(contracts.lpToken)).wait()
-    console.log('Cascade.setLPToken() succeeded')
+    console.log('Cascade.setLPToken(', contracts.lpToken, ') succeeded')
 }
 
 
